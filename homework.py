3e96ca7fe4eb50db580fb680d3831951e8323084
Georgiy <lizgin.gv@phystech.edu>
2021-04-14 21:51:34 +0300

    commit

--- homework.py
+++ homework.py
@@ -24,7 +24,8 @@ def parse_homework_status(homework):
     if status == 'rejected':
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
-        verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
+        verdict = (
+            'Ревьюеру всё понравилось, можно приступать к следующему уроку.')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
 
 
@@ -44,20 +45,23 @@ def send_message(message, bot_client):
 
 
 def main():
-    # проинициализировать бота здесь
     logging.debug('Бот успешно запущен')
     bot = telegram.Bot(token=TELEGRAM_TOKEN)
-    current_timestamp = int(time.time())  # начальное значение timestamp
+    current_timestamp = int(time.time())
     current_timestamp = 0
 
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
-                send_message(parse_homework_status(new_homework.get('homeworks')[0]), bot)
+                send_message(
+                    parse_homework_status(new_homework.get('homeworks')[0]),
+                    bot)
                 logging.info('Сообщение успешно отправлено')
-            current_timestamp = new_homework.get('current_date', current_timestamp)  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+            current_timestamp = new_homework.get(
+                'current_date',
+                current_timestamp)
+            time.sleep(300)
 
         except Exception as e:
             print(f'Бот столкнулся с ошибкой: {e}')
