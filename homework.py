20713122a2153f2e99a500683a1062b6cba21a22
Georgiy <lizgin.gv@phystech.edu>
2021-05-12 18:53:17 +0300

    commit

--- homework.py
+++ homework.py
@@ -16,17 +16,19 @@ logging.basicConfig(
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+STATUSES = {
+    'approved': (
+        'Ревьюеру всё понравилось, '
+        'можно приступать к следующему уроку.'),
+    'rejected': (
+        'К сожалению в работе нашлись ошибки.'),
+}
+HEADERS = {'Authorization': 'OAuth ' + PRAKTIKUM_TOKEN}
+URL = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
 
 
 def parse_homework_status(homework):
     homework_name = homework.get('homework_name')
-    STATUSES = {
-        'approved': (
-            'Ревьюеру всё понравилось, '
-            'можно приступать к следующему уроку.'),
-        'rejected': (
-            'К сожалению в работе нашлись ошибки.'),
-    }
     status = homework.get('status')
     verdict = STATUSES.get(status, 'Неизвестный статус работы')
     return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
@@ -35,14 +37,12 @@ def parse_homework_status(homework):
 def get_homework_statuses(current_timestamp):
     if current_timestamp is None:
         current_timestamp = int(time.time())
-    headers = {'Authorization': 'OAuth ' + PRAKTIKUM_TOKEN}
     params = {'from_date': current_timestamp}
-    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
     try:
         homework_statuses = requests.get(
-            url,
+            URL,
             params=params,
-            headers=headers
+            headers=HEADERS
         )
         return homework_statuses.json()
     except requests.exceptions.RequestException as e:
